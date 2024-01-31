2bab4759a5b650bca9beb301906deb97099d77ce
KeerthiEturi <157362039+KeerthiEturi@users.noreply.github.com>
2024-01-31 16:02:02 +0530

    first commit

--- ifelse.js
+++ ifelse.js
@@ -1,19 +1,19 @@
-function testNum(a) {
-    let result;
-    if(a>0) {
-        result='positive';
-    } else {
-      result='not positive';
-    }
-    return result;
-}
-console.log(testNum(2));
+// function testNum(a) {
+//     let result;
+//     if(a>0) {
+//         result='positive';
+//     } else {
+//       result='not positive';
+//     }
+//     return result;
+// }
+// console.log(testNum(2));
 
 
-//hour
-//If hour is between 6 am and 12pm:Good morning!
-//If it is between 12pm and 6pm :Good afternoon !
-//Otherwise:Good evening!
+ //hour
+ //If hour is between 6 am and 12pm:Good morning!
+ //If it is between 12pm and 6pm :Good afternoon !
+ //Otherwise:Good evening!
 
 let hour=10;
 
@@ -25,3 +25,23 @@ else if (hour>=12 && hour<18) {
 }
 else
 console.log('Good evening');
+
+
+//SWITCH CASE 
+
+ let role;
+switch (role='moderator') {
+    case 'guest':
+    console.log('Guest User');
+    break;
+    case 'moderator':
+    console.log('Moderate User');
+    break;
+    default:
+        console.log('Unknown User');
+}
+
+
+if(role==='guest') console.log('Guest');
+else if(role==='moderator') console.log('Moderator');
+else console.log('Unknown User');
